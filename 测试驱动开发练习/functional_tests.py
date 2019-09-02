373701ac7aa6be9ca0467d53c990838283c07d3d
404235128 <404235128@qq.com>
2019-09-02 16:39:09 +0800

    test

--- 测试驱动开发练习/functional_tests.py
+++ 测试驱动开发练习/functional_tests.py
@@ -3,7 +3,20 @@
 # author: luwh time:2019/8/9
 
 from selenium import webdriver
+import unittest
 
-browser=webdriver.Chrome()
-browser.get('http://localhost:8001')
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser=webdriver.Chrome()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do',self.browser.title)
+        self.fail('finish the test!')
+
+if __name__ == '__main__':
+    unittest.main()
